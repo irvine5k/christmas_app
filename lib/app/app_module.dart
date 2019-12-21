beb95e90782d5a0c5e8fae2a99ab4775b01fe818
Kelven Galvão <kelvengalvao@gmail.com>
2019-12-20 22:21:37 -0300

    switch mobx to flutter_bloc and add assets

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,4 @@
-import 'package:desafio_natal/app/app_controller.dart';
+import 'package:desafio_natal/app/app_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:desafio_natal/app/app_widget.dart';
@@ -7,7 +7,7 @@ import 'package:desafio_natal/app/modules/home/home_module.dart';
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => AppController()),
+        Bind((i) => AppBloc()),
       ];
 
   @override
--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,11 +1,11 @@
-import 'package:desafio_natal/app/modules/home/home_controller.dart';
+import 'package:desafio_natal/app/modules/home/home_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:desafio_natal/app/modules/home/home_page.dart';
 
 class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => HomeController()),
+        Bind((i) => HomeBloc()),
       ];
 
   @override
